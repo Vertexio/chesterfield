63054014055d2d600baf4004b91ee23026d2b1c8
Geoff Flarity <geoff.flarity@gmail.com>
2011-04-22 21:19:30 -0400

    added 'locking' on inserts, if an insert is progress, subsequent inserts get put on the event loop

--- test/bplus_tree.t.js
+++ test/bplus_tree.t.js
@@ -1,1 +1,19 @@
-require('../lib/bplus_tree');
+var BPlusTree = require('../lib/bplus_tree').BPlusTree;
+
+var bplus_tree = new BPlusTree( 'data/bplus_tree', 4096 );
+
+var on_inserted = function on_inserted( err, sequence_number ) {
+    if ( err ) {
+	console.log( err );
+    }
+    else {
+	console.log('ok');
+    }
+	
+};
+//ok
+bplus_tree.insert( 'foo', 'bar', on_inserted );
+//err
+bplus_tree.insert( 'foo', 'bar', on_inserted );
+//ok
+bplus_tree.insert( 'faz', 'bar', on_inserted );
