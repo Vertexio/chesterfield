fa3d0185f7f93f653b85d98e863dc4826f3ac463
Geoff Flarity <geoff.flarity@gmail.com>
2011-04-24 13:05:09 -0400

    made the tests look closer to node-tap style for future conversion over to it

--- test/bplus_tree.t.js
+++ test/bplus_tree.t.js
@@ -2,23 +2,55 @@ var BPlusTree = require('../lib/bplus_tree').BPlusTree;
 
 var bplus_tree = new BPlusTree( 'data/bplus_tree', 4096 );
 
-var on_inserted = function on_inserted( err, sequence_number ) {
-    if ( err ) {
-	console.log( err );
-    }
-    else {
-	console.log('ok');
-    }
-	
-};
-//ok
-bplus_tree.insert( 'foo', 'bar', on_inserted );
-//err
-bplus_tree.insert( 'foo', 'baz', on_inserted );
-//ok
-bplus_tree.insert( 'faz', 'bar', on_inserted );
+//eventually we'll uncomment this once node-tap becomes more mature
+//var test = require("tap").test
 
-//err
-bplus_tree.insert( 'foo', 'bin', on_inserted );
-//err
-bplus_tree.insert( 'foo', 'bak', on_inserted );
+//fake node-tap
+var test = function test( suite_name, on_run_tests ) {
+    
+    console.log( 'Running ' + suite_name );
+
+    var t = {
+	ok : function( is_ok, test_string ) {
+	    if ( is_ok ) {
+		console.log( 'ok' + ' - ' + test_string );
+	    }
+	    else {
+		console.log( 'fail' + ' - ' + test_string );
+	    }
+		
+	}
+    }
+
+    on_run_tests( t );
+    
+};
+
+var on_inserted_generator = function on_inserted_generator( t, should_be_err, test_string ) {
+    
+    var on_inserted = function on_inserted( is_err, sequence_number ) {
+	
+	t.ok( ! is_err && ! should_be_err || is_err && should_be_err,  test_string ); 
+    };
+
+    return on_inserted;
+};
+
+test( 'Basic Tests', function (t) {
+
+	var on_inserted_first_insert = on_inserted_generator( t, false, 'First insert' );
+	bplus_tree.insert( 'foo', 'bar', on_inserted_first_insert );
+
+	var on_inserted_first_dupe = on_inserted_generator( t, true, 'Insert first duplicate key' );
+	bplus_tree.insert( 'foo', 'baz', on_inserted_first_dupe );
+
+	var on_inserted_second_key = on_inserted_generator( t, false, 'Insert second key < first' );
+	//bplus_tree.insert( 'faz', 'bar', on_inserted_second_key );
+	
+	var on_inserted_second_dupe = on_inserted_generator( t, true, 'Second duplicate key' );
+	bplus_tree.insert( 'foo', 'baz', on_inserted_first_dupe );
+	
+    }
+    
+);
+
