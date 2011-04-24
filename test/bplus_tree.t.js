d63d0360371adc25d1e513ccc89e1e633c0fd724
Geoff Flarity <geoff.flarity@gmail.com>
2011-04-24 10:40:53 -0400

    first basic insert tests passing

--- test/bplus_tree.t.js
+++ test/bplus_tree.t.js
@@ -14,6 +14,11 @@ var on_inserted = function on_inserted( err, sequence_number ) {
 //ok
 bplus_tree.insert( 'foo', 'bar', on_inserted );
 //err
-bplus_tree.insert( 'foo', 'bar', on_inserted );
+bplus_tree.insert( 'foo', 'baz', on_inserted );
 //ok
 bplus_tree.insert( 'faz', 'bar', on_inserted );
+
+//err
+bplus_tree.insert( 'foo', 'bin', on_inserted );
+//err
+bplus_tree.insert( 'foo', 'bak', on_inserted );
